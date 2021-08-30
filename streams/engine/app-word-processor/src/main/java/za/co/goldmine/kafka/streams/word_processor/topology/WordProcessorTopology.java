9815f1186a56d4940014ccbc1e9c1839097ee8b8
Bhavesh Sooka <bhavesh@synthesis.co.za>
2021-08-30 10:32:38 +0200

    streams app - word processor

---
+++ streams/engine/app-word-processor/src/main/java/za/co/goldmine/kafka/streams/word_processor/topology/WordProcessorTopology.java
@@ -0,0 +1,48 @@
+package za.co.goldmine.kafka.streams.word_processor.topology;
+
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.Topology;
+import org.springframework.stereotype.Component;
+import za.co.goldmine.kafka.streams.word_processor.streams_builder.WordProcessorBuilder;
+
+import java.util.Properties;
+
+@Component
+public class WordProcessorTopology {
+
+    private final String topicRawData = "inbound-raw-data";
+    private final String topicTitleCase = "outbound-title-case";
+    private final String topicWordCounts = "outbound-word-counts";
+    private final String topicEvenLengthWords = "outbound-even-length-words";
+    private final String topicOddLengthWords = "outbound-odd-length-words";
+
+    public void startStreamsApp() {
+        Topology topology = createTopology();
+        KafkaStreams streams = new KafkaStreams(topology, getStreamsProperties());
+        streams.start();
+    }
+
+    private Topology createTopology() {
+        // Create streams builder
+        StreamsBuilder builder = new WordProcessorBuilder()
+          .setupTitleCaseFlow(topicRawData, topicTitleCase)
+          .setupWordCountFlow(topicRawData, topicWordCounts)
+          .setupWordLengthFlow(topicRawData, topicEvenLengthWords, topicOddLengthWords)
+          .getBuilder();
+
+        return builder.build();
+    }
+
+    private Properties getStreamsProperties() {
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", "kafka-broker1:9092");
+
+        properties.put("application.id", "word-processor-streams-app");
+        properties.put("client.id", "word-processor-streams-client");
+        properties.put("auto.offset.reset", "earliest");
+        properties.put("commit.interval.ms", "100000");
+
+        return properties;
+    }
+}
